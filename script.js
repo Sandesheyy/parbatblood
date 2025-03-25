1049c2d333ad5d63672b6edf12e5b5bc1a3fae17
Sandesh sharma <112399110+Sandesheyy@users.noreply.github.com>
2025-03-25 16:36:15 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openEligibilityPopup() {
+  document.getElementById('eligibility-popup').style.display = 'flex';
+}
+
+function closeEligibilityPopup() {
+  document.getElementById('eligibility-popup').style.display = 'none';
+}
